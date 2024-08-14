d94cb836a79ef6cad037f9e8379718c8ce3d3f8d
Andrii Pylypenko <an.pylypenko@eleks.com>
2024-08-14 17:09:22 +0300

    Initial commit

---
+++ apps/fbs2.0/src/app/app.tsx
@@ -0,0 +1,14 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import styles from './app.module.scss';
+
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <div>
+      <NxWelcome title="fbs2.0" />
+    </div>
+  );
+}
+
+export default App;
