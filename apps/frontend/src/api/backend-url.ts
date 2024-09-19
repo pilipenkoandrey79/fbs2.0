c91cd631e4539b36a64c88c679a282012789e192
Andrii Pylypenko <an.pylypenko@eleks.com>
2024-09-19 09:00:54 +0300

    cleanup

--- apps/frontend/src/api/backend-url.ts
+++ apps/frontend/src/api/backend-url.ts
@@ -5,8 +5,6 @@ const {
   VITE_SSL,
 } = import.meta.env;
 
-console.log(import.meta.env.VITE_JWT_REFRESH_EXPIRATION_TIME);
-
 export default `${
   VITE_SSL === "1" ? "https" : "http"
 }://${VITE_BACKEND_HOST}:${VITE_BACKEND_PORT}${
