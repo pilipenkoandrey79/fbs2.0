26125b9a03be54b56955b091b28245935e798cc0
Andrii Pylypenko <an.pylypenko@eleks.com>
2025-06-23 15:42:57 +0300

    DB dump 23.06.2025

--- apps/frontend2/src/pages/Tournament/components/Participants/components/AddForm/index.tsx
+++ apps/frontend2/src/pages/Tournament/components/Participants/components/AddForm/index.tsx
@@ -1,9 +1,8 @@
 import { ParticipantDto } from "@fbs2.0/types";
-import { Button, Checkbox, Form } from "antd";
+import { Checkbox, Form } from "antd";
 import { FC, useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
 
-import { AddClubForm } from "./components/AddClubForm";
 import { SubmitButton } from "../../../../../../components/SubmitButton";
 import { ParticipantSelector } from "../../../ParticipantSelector";
 import { StageSelector } from "../../../../../../components/selectors/StageSelector";
@@ -24,7 +23,6 @@ const AddForm: FC<Props> = ({ close, selectedCountryId, year }) => {
   const createParticipant = useCreateParticipant();
 
   const [countryId, setCountryId] = useState<number | undefined>();
-  const [isAddClubOpen, setIsAddClubOpen] = useState(false);
   const [addMore, setAddMore] = useState(true);
 
   const submit = async (values: ParticipantDto) => {
@@ -40,12 +38,6 @@ const AddForm: FC<Props> = ({ close, selectedCountryId, year }) => {
   useEffect(() => {
     setCountryId(selectedCountryId);
   }, [selectedCountryId]);
-
-  useEffect(() => {
-    if (!countryId && isAddClubOpen) {
-      setIsAddClubOpen(false);
-    }
-  }, [countryId, isAddClubOpen]);
 
   return (
     <>
@@ -64,39 +56,19 @@ const AddForm: FC<Props> = ({ close, selectedCountryId, year }) => {
           <StageSelector startingStages name="startingStage" />
         </div>
         <div className={styles.buttons}>
-          <span>
-            {!isAddClubOpen && (
-              <div className={styles.submission}>
-                <SubmitButton
-                  form={form}
-                  size="small"
-                  loading={createParticipant.isPending}
-                  className={styles["submission-button"]}
-                />
-                <Checkbox
-                  checked={addMore}
-                  onChange={() => setAddMore(!addMore)}
-                >
-                  {t("common.add_more")}
-                </Checkbox>
-              </div>
-            )}
-          </span>
-          <Button
-            type="link"
-            size="small"
-            onClick={() => setIsAddClubOpen(!isAddClubOpen)}
-            disabled={!countryId}
-          >
-            {isAddClubOpen
-              ? t("common.close")
-              : t("tournament.participants.list.add_club")}
-          </Button>
+          <div className={styles.submission}>
+            <SubmitButton
+              form={form}
+              size="small"
+              loading={createParticipant.isPending}
+              className={styles["submission-button"]}
+            />
+            <Checkbox checked={addMore} onChange={() => setAddMore(!addMore)}>
+              {t("common.add_more")}
+            </Checkbox>
+          </div>
         </div>
       </Form>
-      {isAddClubOpen && countryId && (
-        <AddClubForm countryId={countryId} className={styles["add-club"]} />
-      )}
     </>
   );
 };
